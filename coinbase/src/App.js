7c8bc49b7fe4b3cb0899873edb22230535a1934f
Yahaya <yasholma1@gmail.com>
2020-06-08 17:57:22 +0100

    feat: add rubbish
    feat: learn git

--- coinbase/src/App.js
+++ coinbase/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Button from "./components/Button/Button";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-       Hello
-    </div>
-  );
+    return (
+        <div
+            className='App'
+            style={{ display: "grid", placeContent: "center" }}
+        >
+            <div style={{ width: "200px", height: "200px", background: "red" }}>
+                Hello Team
+                <Button />
+            </div>
+        </div>
+    );
 }
 
 export default App;
